202c260198f0c3bed860c20cff9559f943f9b01f
Gabriel Araújo <gabrielsant95@gmail.com>
2023-04-03 08:47:52 -0300

    Creating basic server paths.

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def contact(request):
+    return HttpResponse('contact')
+
+
+def about(request):
+    return HttpResponse('about')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('contact/', contact),
+    path('about', about),
 ]
