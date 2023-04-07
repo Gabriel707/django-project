4aad6fe8646ccb177ef2ee626621045e8db8b34e
Gabriel Araujo <gabrielsant95@gmail.com>
2023-04-07 20:45:38 -0300

    Editing html files

--- mopinion/views.py
+++ mopinion/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'mopinion/home.html')
+    return render(request, 'mopinion/home.html', context={
+        'name': 'Gabriel Araujo',
+    })
 
 
 def contact(request):
